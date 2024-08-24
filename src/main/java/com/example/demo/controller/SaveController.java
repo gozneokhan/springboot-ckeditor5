e9b75b701caeeeadc3150a79bb6f1fe7c8e75ed2
gozneokhan <gozneokhan@gmail.com>
2024-08-24 16:45:46 +0900

    feat: Controller-Service-DB 코드 작성

--- src/main/java/com/example/demo/controller/SaveController.java
+++ src/main/java/com/example/demo/controller/SaveController.java
@@ -1,16 +1,28 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.SaveDTO;
+import com.example.demo.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SaveController {
 
+    private ContentService contentService;
+
+    @Autowired
+    public SaveController(ContentService contentService) {
+
+        this.contentService = contentService;
+
+    }
+
     @PostMapping("/save")
     public String saveLogic(SaveDTO saveDTO) {
 
-        System.out.println(saveDTO);
+//        System.out.println(saveDTO);
+        contentService.saveContent(saveDTO);
 
         return "redirect:/";
     }
---
+++ src/main/java/com/example/demo/service/ContentService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.SaveDTO;
+import com.example.demo.entity.ContentEntity;
+import com.example.demo.repository.ContentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContentService {
+
+    private ContentRepository contentRepository;
+
+    @Autowired
+    public ContentService(ContentRepository contentRepository) {
+
+        this.contentRepository = contentRepository;
+    }
+
+    public void saveContent(SaveDTO saveDTO) {
+
+        String title = saveDTO.getTitle();
+        String content = saveDTO.getContent();
+
+        ContentEntity contentEntity = new ContentEntity();
+
+        contentEntity.setTitle(title);
+        contentEntity.setContent(content);
+
+        contentRepository.save(contentEntity);
+
+        return;
+    }
+}
